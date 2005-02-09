577edc3d2d51dbeb95ffb5ee07badd63a283fd12
kevinkofler <kevinkofler@9552661e-59e3-4036-b4f2-dbe53926924f>
2005-02-09 00:57:33 +0000

    Update version number to 0.96 Beta 2.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@145 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/tigcc/ide/VersionUnit.pas
+++ trunk/tigcc/ide/VersionUnit.pas
@@ -4,8 +4,8 @@ unit VersionUnit;
 interface
 
 const
-	TIGCCShortVersion = '0.96b1';
-	TIGCCLongVersion = '0.96 Beta 1';
+	TIGCCShortVersion = '0.96b2';
+	TIGCCLongVersion = '0.96 Beta 2';
 
 implementation
 
