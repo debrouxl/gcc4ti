c6b3bfed90180fcf5c8da0d16ecbb595d61c2e60
kevinkofler <kevinkofler@9552661e-59e3-4036-b4f2-dbe53926924f>
2005-03-03 19:00:15 +0000

    Regenerate setjmp.h.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@176 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/tigcc/include/C/setjmp.h
+++ trunk/tigcc/include/C/setjmp.h
@@ -5,7 +5,7 @@
 
 /* Begin Auto-Generated Part */
 typedef struct{unsigned long D2,D3,D4,D5,D6,D7;unsigned long A2,A3,A4,A5,A6,A7;unsigned long PC;}JMP_BUF[1];
-#define longjmp _rom_call(void,(void*,short),267)
+#define longjmp _rom_call_attr(void,(void*,short),__attribute__((__noreturn__)),267)
 #define setjmp _rom_call(short,(void*),266)
 /* End Auto-Generated Part */
 
