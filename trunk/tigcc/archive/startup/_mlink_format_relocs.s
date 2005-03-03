5f6d93ca7e0c09258a081d4ebf5be5ab1e663672
kevinkofler <kevinkofler@9552661e-59e3-4036-b4f2-dbe53926924f>
2005-03-03 18:24:26 +0000

    Fix format description.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@173 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/tigcc/archive/startup/_mlink_format_relocs.s
+++ trunk/tigcc/archive/startup/_mlink_format_relocs.s
@@ -39,56 +39,21 @@ __mlink_format_relocs_done_2:
 
 __ld_insert_mlink_relocs:
 | The format for the reloc table is as follows:
-/* The offset starts at __ld_entry_point - 4 bytes (the smallest encodable
-   difference).
-00: end of list
-01-7F: add 004-100 (2(value+1)) to the previous offset to get the new one
-80-BF: add 04-22 (2(nibble_value+2)). (Note that 01-7F can be used for
-       that purpose as well if you are a lazy linker author. :-) But the
-       nibble-encoding saves some space, so you'd better use it.)
-       The bytes have the following values (the most significant nibble is
-       the first one):
-       Byte 1: n+6 (2+6=8 .. 5+6=B), first data nibble
-       Byte 2: 2 data nibbles
-       ...
-       Byte n: 2 data nibbles
-       Note that n = ([total number of data nibbles] + 1) / 2. It follows
-       that the total number of data nibbles must be odd and >=3. Use
-       byte-encoding (01-7F) otherwise (for all data if the number is <=2,
-       for the last data nibble only if it is even and >=4). It also must
-       not exceed 9 (5*2-1), in which case you need to split it up.
-C0-FF: the byte starts a full word: 0xC000+difference
-       add 2(difference+0x81) to the previous offset to get the new one
-       FFFF is special: it adds 0x80FC to the offset, but the computation
-                        does not stop here, instead another delta is added
+/*
+Reloc offsets are encoded by bytes using the following structure:
+1 bit: stop bit
+7 bits: data
 
-(NOTE: numbers below are decimal unless prefixed by "0x"!)
-In pseudo-code, this means:
-offset=__ld_entry_point-4;
-for each (byte) {
-  switch (byte) {
-    case 0: stop;
-    case 0x01..0x7f:
-      offset+=2*(byte+1);
-      dump(offset);
-      break;
-    case 0x80..0xbf:
-      n=(byte>>4)-6;
-      offset+=2*((byte&7)+2);
-      dump(offset);
-      for(i=1;i<n;i++) {
-        offset+=2*((byte>>4)+2);
-        dump(offset);
-        offset+=2*((byte&7)+2);
-        dump(offset);
-      }
-    case 0xc0..0xff:
-      complete the word;
-      if (word==0xFFFF) {
-        offset+=0x80FC;
-      } else {
-        offset+=2*(word-(0xC000-0x81));
-        dump(offset);
-      }
-   }
-} */
+The offset is split in 7-bit chunks. The last bit is thrown away, so an offset
+<256 fits into one byte despite the stop bit. In other words, the encoding is
+as follows:
+offset := actual_offset/2
+* 0 <= offset < 128: 1 byte: 0x80+offset
+* 128 <= offset < 16384: 2 bytes: offset/128, 0x80+(offset%128)
+* 16384 <= offset < 2097152: 3 bytes: offset/16384, (offset%16384)/128,
+  0x80+(offset%128)
+(Anything higher isn't interesting for us because of the 64 KB file size
+limit.)
+
+A reloc table is a list of such encoded offsets terminated by a 0x00 byte.
+*/
