65856419cc56315406ae5d8336f701914af2e5f7
kevinkofler <kevinkofler@9552661e-59e3-4036-b4f2-dbe53926924f>
2006-03-04 14:16:56 +0000

    Change jsr to jbsr.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@488 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/tigcc/archive/trampoffset.s
+++ trunk/tigcc/archive/trampoffset.s
@@ -1,7 +1,7 @@
 | Function to compute the offset for nested function trampolines. Also handles
 | the automatical global import of EXECUTE_IN_GHOST_SPACE (i.e. ghost space
 | execution for HW2, HW3Patch detection for HW3).
-| Copyright (C) 2003 Kevin Kofler.
+| Copyright (C) 2003, 2006 Kevin Kofler.
 | See License.txt for licensing conditions.
 
 | This object file needs EXECUTE_IN_GHOST_SPACE.
@@ -10,7 +10,7 @@
 	.xdef __trampoline_offset
 
 __trampoline_offset:
-	jsr __get_hw_version
+	jbsr __get_hw_version
 	moveq.l #0,%d1
 	subq.w #2,%d0
 	bne.s 0f
