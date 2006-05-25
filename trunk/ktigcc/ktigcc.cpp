f37c6d879858e9aca36c9c262a31b176e54c6a55
kevinkofler <kevinkofler@9552661e-59e3-4036-b4f2-dbe53926924f>
2006-05-25 11:03:57 +0000

    Update/add copyright notices.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@507 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/ktigcc/ktigcc.cpp
+++ trunk/ktigcc/ktigcc.cpp
@@ -2,6 +2,7 @@
    ktigcc - TIGCC IDE for KDE
 
    Copyright (C) 2004-2006 Kevin Kofler
+   Copyright (C) 2006 Joey Adams
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,7 +16,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
 */
 
 #include <cstdio>
