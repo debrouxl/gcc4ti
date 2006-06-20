4df16d431a6af443e681de85af44fa5f263ecc9d
kevinkofler <kevinkofler@9552661e-59e3-4036-b4f2-dbe53926924f>
2006-06-20 17:16:59 +0000

    Add proper copyright&notices to all IDE .pas source files. (The "GPL version 2 or later" clause was OKed by both Sebastian and Ephyx.)
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@572 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/tigcc/ide/VersionUnit.pas
+++ trunk/tigcc/ide/VersionUnit.pas
@@ -1,10 +1,31 @@
+{
+  TIGCC IDE
+
+  Copyright (C) 2000-2004 Sebastian Reichelt
+  Copyright (C) 2005-2006 Kevin Kofler
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+}
+
 unit VersionUnit;
 
 interface
 
 const
-	TIGCCShortVersion = '0.96b6';
-	TIGCCLongVersion = '0.96 Beta 6';
+	TIGCCShortVersion = '0.96b7';
+	TIGCCLongVersion = '0.96 Beta 7';
 
 implementation
 
