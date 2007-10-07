2c85385dbe3ddbc255d396c5cf55746f96126e04
kevinkofler <kevinkofler@9552661e-59e3-4036-b4f2-dbe53926924f>
2007-10-07 02:06:45 +0000

    Update copyright date.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@1214 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/tigcc/ld-tigcc/manip.c
+++ trunk/tigcc/ld-tigcc/manip.c
@@ -1,7 +1,7 @@
 /* manip.c: Routines to manipulate the internal data
 
    Copyright (C) 2002-2004 Sebastian Reichelt
-   Copyright (C) 2003-2005 Kevin Kofler
+   Copyright (C) 2003-2007 Kevin Kofler
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
