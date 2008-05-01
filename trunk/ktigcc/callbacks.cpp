aae8f00c26a6e0a88a702d8d353ae68e4a17eb9d
kevinkofler <kevinkofler@9552661e-59e3-4036-b4f2-dbe53926924f>
2008-05-01 17:35:31 +0000

    Update copyright date.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@1250 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/ktigcc/callbacks.cpp
+++ trunk/ktigcc/callbacks.cpp
@@ -1,7 +1,7 @@
 /*
    ktigcc - TIGCC IDE for KDE
 
-   Copyright (C) 2006-2007 Kevin Kofler
+   Copyright (C) 2006-2008 Kevin Kofler
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
