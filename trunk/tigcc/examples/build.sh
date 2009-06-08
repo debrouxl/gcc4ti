099c4a9b7bbb5827e1c9256795fde680a5ca9b2c
debrouxl <debrouxl@9552661e-59e3-4036-b4f2-dbe53926924f>
2009-06-08 18:48:00 +0000

    Fix an example whose build has failed since an untested modification done in 2005. While at it:     * add the script used for checking buildability of the examples, and the script used to generate the checker script;     * fix a typo in SIZED_DIALOG.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@1324 9552661e-59e3-4036-b4f2-dbe53926924f

---
+++ trunk/tigcc/examples/build.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+tprbuilder -q "Add Arguments.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Add Arguments.tpr\""; fi
+tprbuilder -q "Adjust Grayscale.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Adjust Grayscale.tpr\""; fi
+tprbuilder -q "Approximation Mode.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Approximation Mode.tpr\""; fi
+tprbuilder -q "Argument Test.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Argument Test.tpr\""; fi
+tprbuilder -q "Basecode Parameters.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Basecode Parameters.tpr\""; fi
+tprbuilder -q "Big Numbers.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Big Numbers.tpr\""; fi
+tprbuilder -q "Bitmap Test.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Bitmap Test.tpr\""; fi
+tprbuilder -q "Catalog.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Catalog.tpr\""; fi
+tprbuilder -q "Cave Blaster.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Cave Blaster.tpr\""; fi
+tprbuilder -q "Create Variable.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Create Variable.tpr\""; fi
+tprbuilder -q "Custom DLL Test.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Custom DLL Test.tpr\""; fi
+tprbuilder -q "Custom DLL.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Custom DLL.tpr\""; fi
+tprbuilder -q "Dialog Items AMS1.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Dialog Items AMS1.tpr\""; fi
+tprbuilder -q "Dialog Items.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Dialog Items.tpr\""; fi
+tprbuilder -q "Dialog Struct.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Dialog Struct.tpr\""; fi
+tprbuilder -q "Dialog Test.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Dialog Test.tpr\""; fi
+tprbuilder -q "Draw Line.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Draw Line.tpr\""; fi
+tprbuilder -q "Dynamic Matrix.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Dynamic Matrix.tpr\""; fi
+tprbuilder -q "Dynamic Popup Example.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Dynamic Popup Example.tpr\""; fi
+tprbuilder -q "Flags of WinStrXYWrap.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Flags of WinStrXYWrap.tpr\""; fi
+tprbuilder -q "Float Test.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Float Test.tpr\""; fi
+tprbuilder -q "Folder List.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Folder List.tpr\""; fi
+tprbuilder -q "Folder.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Folder.tpr\""; fi
+tprbuilder -q "Function Returning Itself.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Function Returning Itself.tpr\""; fi
+tprbuilder -q "Get File Name.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Get File Name.tpr\""; fi
+tprbuilder -q "Graph Function.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Graph Function.tpr\""; fi
+tprbuilder -q "Gray Test Project in Assembler.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Gray Test Project in Assembler.tpr\""; fi
+tprbuilder -q "Gray Test Project.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Gray Test Project.tpr\""; fi
+tprbuilder -q "Guess.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Guess.tpr\""; fi
+tprbuilder -q "Handle a variable with VAT functions.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Handle a variable with VAT functions.tpr\""; fi
+tprbuilder -q "Hardware Parameters.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Hardware Parameters.tpr\""; fi
+tprbuilder -q "Heap Shuffle.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Heap Shuffle.tpr\""; fi
+tprbuilder -q "Hello World 1.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Hello World 1.tpr\""; fi
+tprbuilder -q "Hello World 2.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Hello World 2.tpr\""; fi
+tprbuilder -q "Hello World 3.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Hello World 3.tpr\""; fi
+tprbuilder -q "Hello World 4.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Hello World 4.tpr\""; fi
+tprbuilder -q "Hello World 5.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Hello World 5.tpr\""; fi
+tprbuilder -q "Input String Advanced.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Input String Advanced.tpr\""; fi
+tprbuilder -q "Input String.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Input String.tpr\""; fi
+tprbuilder -q "Integrate.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Integrate.tpr\""; fi
+tprbuilder -q "Interrupt Handler.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Interrupt Handler.tpr\""; fi
+tprbuilder -q "Key Yes or No.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Key Yes or No.tpr\""; fi
+tprbuilder -q "Launcher.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Launcher.tpr\""; fi
+tprbuilder -q "List variables and folders.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"List variables and folders.tpr\""; fi
+tprbuilder -q "Masked Sprite.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Masked Sprite.tpr\""; fi
+tprbuilder -q "Memory Error.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Memory Error.tpr\""; fi
+tprbuilder -q "Menu Example 1.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Menu Example 1.tpr\""; fi
+tprbuilder -q "Menu Example 2.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Menu Example 2.tpr\""; fi
+tprbuilder -q "Multiply Polynoms.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Multiply Polynoms.tpr\""; fi
+tprbuilder -q "Othello-Reversi.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Othello-Reversi.tpr\""; fi
+tprbuilder -q "Popup Menu Example.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Popup Menu Example.tpr\""; fi
+tprbuilder -q "Pretty Print.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Pretty Print.tpr\""; fi
+tprbuilder -q "Print EStack.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Print EStack.tpr\""; fi
+tprbuilder -q "Progress Bar.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Progress Bar.tpr\""; fi
+tprbuilder -q "Sort Floats.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Sort Floats.tpr\""; fi
+tprbuilder -q "Sort Integers.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Sort Integers.tpr\""; fi
+tprbuilder -q "Static Expression.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Static Expression.tpr\""; fi
+tprbuilder -q "Static Popup.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Static Popup.tpr\""; fi
+tprbuilder -q "Symbolic Add Arguments.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Symbolic Add Arguments.tpr\""; fi
+tprbuilder -q "Text Editor.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Text Editor.tpr\""; fi
+tprbuilder -q "Timers.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Timers.tpr\""; fi
+tprbuilder -q "Window 1.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Window 1.tpr\""; fi
+tprbuilder -q "Window 2.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Window 2.tpr\""; fi
+tprbuilder -q "Window 3.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Window 3.tpr\""; fi
+tprbuilder -q "Window 4.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Window 4.tpr\""; fi
+tprbuilder -q "Window 5.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Window 5.tpr\""; fi
+tprbuilder -q "Windows.tpr"; if [ $? -ne 0 ]; then echo FAILED building "\"Windows.tpr\""; fi
+rm *.??z
+rm *.??y
---
+++ trunk/tigcc/examples/generatebuild.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ls *.tpr | awk 'BEGIN {print "#!/bin/sh"} {print "tprbuilder -q \"" $0 "\"; if [ $? -ne 0 ]; then echo FAILED building \"\\\"" $0 "\\\"\"; fi"} END{print "rm *.??z"; print "rm *.??y"}' > build.sh
