1946f2d64e3eff4e3d0f07515cefbdf637023da1
debrouxl <debrouxl@9552661e-59e3-4036-b4f2-dbe53926924f>
2009-06-30 18:37:56 +0000

    Windows setup scripts: upgrade. I could swear that I had already done those modifications (especially CVS -> .svn), but they somehow got lost.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@1349 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/tigcc/setup/CreateSourceZip.sh
+++ trunk/tigcc/setup/CreateSourceZip.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 ZIPDIR=`pwd`/../../../gcc4ti-source
-A68KSRCDIR=~/a68k/source
 
 rm -f "$ZIPDIR/gcc4tisrc.zip"
 mkdir srczip
 mkdir srczip/doc
 cp -rfp ../doc .
 cd doc
-find . -name CVS -exec rm -rf "{}" \;
-zip -9 -r "../srczip/doc/doc.zip" Programs System -x CVS/\*
+find . -name .svn -exec rm -rf "{}" \;
+zip -9 -r "../srczip/doc/doc.zip" Programs System -x .svn/\*
 cd ..
 rm -rf doc
-mkdir srczip/a68k
-cp -p -f "$A68KSRCDIR"/* srczip/a68k/
-cp -rfp ../Readme.txt ../License.txt ../archive ../components ../gcc ../general ../hsf2rc ../ide ../ld-tigcc ../pstarter ../setup ../tigcc ../tools ../tprbuilder srczip/
+cp -rfp ../../external/a68k ../Readme.txt ../License.txt ../archive ../components ../gcc ../general ../hsf2rc ../ide ../ld-tigcc ../pstarter ../setup ../tigcc ../tools ../tprbuilder srczip/
 cd srczip
-find . -name CVS -exec rm -rf "{}" \;
-zip -9 -r "$ZIPDIR/gcc4tisrc.zip" Readme.txt License.txt a68k archive components doc/doc.zip gcc general hsf2rc ide ld-tigcc pstarter setup/*.nsi setup/*.ini setup/*.bat setup/*.sh setup/MAKEALL.PIF tigcc tools tprbuilder -x CVS/\*
+find . -name .svn -exec rm -rf "{}" \;
+zip -9 -r "$ZIPDIR/gcc4tisrc.zip" Readme.txt License.txt a68k archive components doc/doc.zip gcc general hsf2rc ide ld-tigcc pstarter setup/*.nsi setup/*.ini setup/*.bat setup/*.sh setup/MAKEALL.PIF tigcc tools tprbuilder -x .svn/\*
 cd ..
 rm -rf srczip
 
