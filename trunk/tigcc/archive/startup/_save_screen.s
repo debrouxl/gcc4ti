77331f3e294513a793ed2dbf8d17622966b919a8
debrouxl <debrouxl@9552661e-59e3-4036-b4f2-dbe53926924f>
2009-06-30 17:30:31 +0000

    tigcclib: optimize (-16 bytes) the SAVE_SCREEN implementation (Martial Demolins). Most examples are testcases for this feature.
    git-svn-id: svn+ssh://trac.godzil.net/gcc4ti@1338 9552661e-59e3-4036-b4f2-dbe53926924f

--- trunk/tigcc/archive/startup/_save_screen.s
+++ trunk/tigcc/archive/startup/_save_screen.s
@@ -3,35 +3,29 @@
 | This file requires cleanup code.
 	.xdef __ref_all___complex_main
 
+| Screen save & restore used to be done by calling memcpy, even if doing so
+| yields an executable 16 bytes larger (though a very tiny fraction of a
+| second faster). SAVE_SCREEN is present in most AMS native programs, so its
+| implementation should be focused on a low footprint.
+
 .section _st110
 __save_screen:
-| Save %a2 since we may not destroy it when we return to the AMS.
-	pea.l (%a2)
-| Get LCD_SIZE bytes on the stack.
-	lea.l (%sp,-3840),%sp
-| Push LCD_SIZE (size parameter).
-	pea.l 3840
-| Push LCD_MEM (source parameter).
-	pea.l 0x4C00
-| Push pointer to the space on the stack (dest parameter).
-	pea.l (%sp,8)
-
-| Code to load memcpy into %a2 is inserted here.
-
-.section _st112
-	jsr (%a2)
+| Execute the loop LCD_SIZE/4 times (4 bytes are transferred at a time).
+	move.w #(3840/4)-1,%d0
+| LCD_MEM.
+	lea.l 0x4c00.w,%a0
+| Push data on the stack.
+0:
+	move.l (%a0)+,-(%sp)
+	dbf.w %d0,0b
 
 .section _st1050
 __restore_screen:
-| Push LCD_SIZE (size parameter).
-	pea.l 3840
-| Push pointer to saved data on stack (source parameter).
-	pea.l (%sp,16)
-| Push LCD_MEM (dest parameter).
-	pea.l 0x4C00
-| %a2 is still a pointer to memcpy since the function wasn't allowed to destroy it.
-	jsr (%a2)
-| Clean up the stack (data and parameters).
-	lea.l (%sp,3864),%sp
-| Restore %a2 for the AMS.
-	movea.l (%sp)+,%a2
+| Execute the loop LCD_SIZE/4 times.
+	move.w #(3840/4)-1,%d0
+| LCD_MEM + LCD_SIZE.
+	lea.l 0x5b00.w,%a0
+| Pop data from the stack.
+0:
+	move.l (%sp)+,-(%a0)
+	dbf.w %d0,0b
